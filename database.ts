69761ec25c4caec4e1739e5fd9db6ff107712401
yasindu <yazindu@gmail.com>
2023-11-03 01:30:40 +0530

    feat: database logic completed

--- database.ts
+++ database.ts
@@ -1,17 +1,38 @@
-import mysql from 'mysql2'
+import mysql, {PoolOptions, RowDataPacket, ResultSetHeader} from 'mysql2';
 import dotenv from 'dotenv'
+
 dotenv.config()
 
-const pool = mysql.createPool({
+const access: PoolOptions = {
     host: process.env.MYSQL_HOST,
     user: process.env.MYSQL_USER,
     password: process.env.MYSQL_PASSWORD,
     database: process.env.MYSQL_DATABASE
-}).promise()
+};
 
-const getNotes = async () => {
-    const [rows] = await pool.query('select * from notes')
+const conn = mysql.createPool(access).promise();
+
+export const getNotes = async () => {
+    const [rows] = await conn.query('select * from notes')
     return rows
 }
+export const getNote = async (id: string) => {
+    const [row] = await conn.query(`
+        select *
+        from notes
+        where id = ?
+    `, [id])
+    return (row as RowDataPacket)[0]
+}
+export const createNote = async (title: string, contents: string) => {
+    const [result] = await conn.query(`
+        insert into notes(title, contents)
+        values (?, ?)
+    `, [title, contents])
+    const id = (result as ResultSetHeader).insertId
+    return getNote(id.toString())
+}
 
-getNotes().then(notes => console.log(notes))
+// getNotes().then(notes => console.log('getNotes()', notes))
+// getNote('3').then(note => console.log('getNote()', note))
+// createNote('name', 'samantha').then(result => console.log('createNote', result))
