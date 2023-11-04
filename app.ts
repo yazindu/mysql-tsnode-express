50817de3535df628411d4a85060768ec12cebe2c
yasindu <yazindu@gmail.com>
2023-11-04 10:20:22 +0530

    feat: finished tutorial

--- app.ts
+++ app.ts
@@ -1,29 +1,34 @@
-import express from 'express'
+import express, {Request, Response, NextFunction} from 'express'
 import {createNote, getNote, getNotes} from "./database";
+
 const app = express()
 app.use(express.json())
 
-app.get('/notes', async (req, res)=>{
+app.get('/notes', async (req, res) => {
     const notes = await getNotes();
     res.send(notes)
 })
-app.get('/notes/:id', async (req, res)=>{
+app.get('/notes/:id', async (req, res) => {
     const id = req.params.id
     const note = await getNote(id);
     res.send(note)
 })
 
-app.post('/notes', async(req, res)=>{
-    const {title contents} = req.body
-    const note = await createNote(title. contents)
+app.post('/notes', async (req, res) => {
+    const {title, contents} = req.body
+    const note = await createNote(title, contents)
     res.status(201).send(note)
 })
 
-// app.use((err, req, res, next) => {
-//     console.error(err.stack)
-//     res.status(500).send('Something broke!')
-// })
+app.get('/error', (req, res, next) => {
+    throw new Error('This is an intentional error!');
+});
 
-app.listen(8080, ()=>{
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+    console.error(err.stack)
+    res.status(500).send('Something broke!💩')
+})
+
+app.listen(8080, () => {
     console.log('Server is running on port 8080')
 })
